cdf58d9c3a066cbd766fbc0f72abc003e09b5913
Utkarsh raj <42980787+utkarshai@users.noreply.github.com>
2019-07-14 10:33:28 +0530

    Add files via upload

---
+++ j4/js/j4.js
@@ -0,0 +1,27 @@
+var  d, h, m, s, clock, color;
+document.getElementById("new1")="hello";
+function clocky() {
+    d=new Date();
+    h=d.getHours();
+    m=d.getMinutes();
+    s=d.getSeconds();
+    
+    if (h<=9){
+        h='0' + h;
+    }
+    if (m<=9){
+        m='0' + m;
+    }
+    if (s<=9){
+        s='0' + s;
+    }
+    clock = h + ":" + m + ":" + s;
+    color="#" + h + m +s;
+
+    document.getElementById("clock").innerHTML = clock;
+    document.body.style.background = color;
+    
+    setTimeout(clocky, 1000);
+}
+
+clocky();
