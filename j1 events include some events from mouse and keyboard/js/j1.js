63327c085ec90a45d0e5e73b43d2af023ba1ff56
Utkarsh raj <42980787+utkarshai@users.noreply.github.com>
2019-07-14 10:31:53 +0530

    Add files via upload

---
+++ j1 events include some events from mouse and keyboard/js/j1.js
@@ -0,0 +1,28 @@
+function showDate() {
+    document.getElementById("test").innerHTML = Date();
+}
+
+function myFunction(val) {
+    alert("the input data has been changed. the new value is:" + val);
+}
+
+function bigImg(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+}
+
+function normalImg(x) {
+    x.style.height="80px";
+    x.style.width="80px";
+}
+
+function fun2() {
+    alert("you pressed a key  inside the input feild");
+}
+
+function fun3() {
+    alert("you passed a key inside input feild");
+}
+
+
+//document.getElementById("test").innerHTML=
